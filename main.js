e8ba9068d88e6f3e0ec41308ff846c9d3bf54af6
Jamieson <158116310+ash-comsci@users.noreply.github.com>
2024-10-22 10:31:32 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+/**
+ * 2.4 - Custom Functions
+ * ICS3 - Mr. J
+ * 
+ * Read README.md and YOUR_Task.md 
+ * 
+ * This is a playground. Feel free to play.
+ **/ 
+
+
+// Example custom function using Number() and prompt(), must add code to line 13 to acuire input from user
+function circle_area() {
+   
+    console.log(`The area is: ${Math.PI * radius**2}`);
+}
+
+/*****  Your code goes below  *****/
+
